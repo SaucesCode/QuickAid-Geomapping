16fad7a1118196370f74309ff4590841f370abec
Myheadhurts16 <jamessdemesa@gmail.com>
2025-11-01 01:35:52 +0800

    dockerize system

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 import { jwtDecode } from "jwt-decode";
 
-// export const API_URL = process.env.REACT_APP_DJANGO_URL;
-export const API_URL = "http://127.0.0.1:8000/api";
+export const API_URL = process.env.REACT_APP_API_URL;
+// export const API_URL = "http://127.0.0.1:8000/api";
 
 export const api = axios.create({
   baseURL: API_URL,
