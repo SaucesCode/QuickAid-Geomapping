ff609759bb1344c1bcd977aeb760d894d4d2fd1c
James Patrick De Mesa <83571819+SaucesCode@users.noreply.github.com>
2025-11-12 16:25:22 +0800

    Update StaffUtils.js

--- frontend/src/utils/StaffUtils.js
+++ frontend/src/utils/StaffUtils.js
@@ -3,5 +3,5 @@ export const getStaffFormLink = () => {
   const user = JSON.parse(localStorage.getItem("userData"));
   if (!user) return null;
 
-  return `${process.env.REACT_APP_BASE_URL}/new-applicant?staff_ref_code=${user.ref_code}`;
+  return `/new-applicant?staff_ref_code=${user.ref_code}`;
 };
