d54bbe3f3062bcc657137efe0fa91126fb4e9f73
James Patrick De Mesa <83571819+SaucesCode@users.noreply.github.com>
2026-05-11 11:05:24 +0800

    Update 0017_approvalbatch_file_hash.py

--- backend/api/migrations/0017_approvalbatch_file_hash.py
+++ backend/api/migrations/0017_approvalbatch_file_hash.py
@@ -13,7 +13,7 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name='approvalbatch',
             name='file_hash',
-            field=models.CharField(default=0, max_length=64, unique=True),
+            field=models.CharField(max_length=64, unique=True, null=True, blank=True,),
             preserve_default=False,
         ),
     ]
